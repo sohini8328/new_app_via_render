2b940d9a631d60348ac8377c03fa26da88290038
sohini8328 <sohini.tomar@gmail.com>
2024-09-25 19:50:16 -0500

    updated st.slider

--- app.py
+++ app.py
@@ -35,8 +35,3 @@ def toss_coin(n):
 
     return mean
 
-number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
-start_button = st.button('Run')
-
-if start_button:
-    st.write(f'Running the experient of {number_of_trials} trials.')
